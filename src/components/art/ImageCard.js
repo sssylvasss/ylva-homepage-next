f2df6057459354bbb69a9d968236b9492398651e
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2023-03-15 19:23:53 +0100

    style images

--- src/components/art/ImageCard.js
+++ src/components/art/ImageCard.js
@@ -1,56 +1,56 @@
-import React from "react";
-import { any } from "prop-types";
+import React from 'react';
+import { any } from 'prop-types';
 
 import {
-  CardDivMain,
-  CollageImage,
-  TitleH2,
-  TextP,
-  TextDiv,
-  TextDiv1,
-  TableDiv,
-  LaptopDiv,
-} from "./StylingArt";
+	CardDivMain,
+	CollageImage,
+	TitleH2,
+	TextP,
+	TextDiv,
+	TextDiv1,
+	TableDiv,
+	LaptopDiv,
+} from './StylingArt';
 
 export const ImageCard = ({ collage, openModal }) => {
-  return (
-    <>
-      <CardDivMain
-        key={collage?.collageId}
-        onClick={() => openModal(collage?.collageId)}
-      >
-        <CollageImage
-          alt={collage.collageTitle}
-          src={collage.collageImage?.file.url}
-        />
-        <TableDiv>
-          <TextDiv1>
-            <TitleH2>{collage.collageTitle},</TitleH2>
-            {collage.size ? <TextP>{collage.size}cm.</TextP> : null}
-          </TextDiv1>
-        </TableDiv>
-        <LaptopDiv>
-          <TextDiv>
-            <TitleH2>{collage.collageTitle}</TitleH2>
-            {collage.size ? <TextP>{collage.size}cm.</TextP> : null}
+	return (
+		<>
+			<CardDivMain
+				key={collage?.collageId}
+				onClick={() => openModal(collage?.collageId)}
+			>
+				<CollageImage
+					alt={collage.collageTitle}
+					src={collage.collageImage?.file.url}
+				/>
+				<TableDiv>
+					<TextDiv1>
+						<TitleH2>{collage.collageTitle},</TitleH2>
+						{collage.size ? <TextP>{collage.size}cm.</TextP> : null}
+					</TextDiv1>
+				</TableDiv>
+				<LaptopDiv>
+					<TextDiv>
+						<TitleH2>{collage.collageTitle}</TitleH2>
+						{collage.size ? <TextP>{collage.size}cm.</TextP> : null}
 
-            {collage.serie ? (
-              <>
-                <TextP>
-                  From the serie {collage.serie}, {collage.year}
-                </TextP>
-              </>
-            ) : (
-              <TextP>{collage.year}</TextP>
-            )}
-          </TextDiv>
-        </LaptopDiv>
-      </CardDivMain>
-    </>
-  );
+						{collage.serie ? (
+							<>
+								<TextP>
+									From the serie {collage.serie}, {collage.year}
+								</TextP>
+							</>
+						) : (
+							<TextP>{collage.year}</TextP>
+						)}
+					</TextDiv>
+				</LaptopDiv>
+			</CardDivMain>
+		</>
+	);
 };
 
 ImageCard.propTypes = {
-  collage: any,
-  openModal: any,
+	collage: any,
+	openModal: any,
 };
--- src/pages/collage.js
+++ src/pages/collage.js
@@ -1,138 +1,138 @@
-import React, { useState, useEffect, Fragment } from "react";
-import { useContentful } from "../useContentful";
-import { ImageCard } from "../Components/art/ImageCard";
-import ImageList from "@mui/material/ImageList";
-import ImageListItem from "@mui/material/ImageListItem";
+import React, { useState, useEffect, Fragment } from 'react';
+import { useContentful } from '../useContentful';
+import { ImageCard } from '../Components/art/ImageCard';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
 
 import {
-  Main,
-  ImageSectionDiv,
-  TitleH1,
-  ImageSectionInnerDiv,
-  ModalImage,
-  ArrowForward,
-  ArrowBack,
-  ModalDiv,
-  LaptopDiv,
-  TableDiv,
-} from "../Components/art/StylingArt";
-import { Modal } from "@/components/modal/Modal";
+	Main,
+	ImageSectionDiv,
+	TitleH1,
+	ImageSectionInnerDiv,
+	ModalImage,
+	ArrowForward,
+	ArrowBack,
+	ModalDiv,
+	LaptopDiv,
+	TableDiv,
+} from '../Components/art/StylingArt';
+import { Modal } from '@/components/modal/Modal';
 
 export const Collage = () => {
-  const [collages, setCollages] = useState([]);
-  const { getCollage } = useContentful();
-  const [showModal, setShowModal] = useState(false);
-  const [activeCollage, setActiveCollage] = useState();
+	const [collages, setCollages] = useState([]);
+	const { getCollage } = useContentful();
+	const [showModal, setShowModal] = useState(false);
+	const [activeCollage, setActiveCollage] = useState();
 
-  useEffect(() => {
-    getCollage().then((response) => {
-      setCollages(response?.sort((a, b) => a.collageId - b.collageId));
-    });
-  }, []);
+	useEffect(() => {
+		getCollage().then((response) => {
+			setCollages(response?.sort((a, b) => a.collageId - b.collageId));
+		});
+	}, []);
 
-  // acc= acumilera(ansamla). reduce kan ha upp till 4 parametrar, index är nr 3
-  const newImageArray = collages
-    .sort((a, b) => b.collageId - a.collageId)
-    .reduce((acc, curr) => {
-      if (acc.some(({ serie }) => serie === curr.serie)) {
-        acc.map((accElement) => {
-          if (accElement.serie === curr.serie) {
-            accElement.collages.push(curr);
-          }
-        });
-        return acc;
-      } else {
-        const obj = {
-          serie: curr.serie ? curr.serie : curr.year,
-          year: curr.serie ? curr.year : null,
-          collages: [curr],
-        };
-        return [...acc, obj];
-      }
-      // initial value nedan
-    }, []);
+	// acc= acumilera(ansamla). reduce kan ha upp till 4 parametrar, index är nr 3
+	const newImageArray = collages
+		.sort((a, b) => b.collageId - a.collageId)
+		.reduce((acc, curr) => {
+			if (acc.some(({ serie }) => serie === curr.serie)) {
+				acc.map((accElement) => {
+					if (accElement.serie === curr.serie) {
+						accElement.collages.push(curr);
+					}
+				});
+				return acc;
+			} else {
+				const obj = {
+					serie: curr.serie ? curr.serie : curr.year,
+					year: curr.serie ? curr.year : null,
+					collages: [curr],
+				};
+				return [...acc, obj];
+			}
+			// initial value nedan
+		}, []);
 
-  const openModal = (id) => {
-    const activeCollageIndex = collages.findIndex((co) => co.collageId === id);
-    setActiveCollage(collages[activeCollageIndex]);
-    console.log("id", id);
-    setShowModal(true);
-  };
+	const openModal = (id) => {
+		const activeCollageIndex = collages.findIndex((co) => co.collageId === id);
+		setActiveCollage(collages[activeCollageIndex]);
+		console.log('id', id);
+		setShowModal(true);
+	};
 
-  const imageSlide = (next) => {
-    const imageIndex = collages.findIndex(
-      (co) => co.collageId === activeCollage.collageId
-    );
-    if (next) {
-      if (imageIndex === collages.length - 1) {
-        setActiveCollage(collages[0]);
-      } else {
-        setActiveCollage(collages[imageIndex + 1]);
-      }
-    } else {
-      if (imageIndex === 0) {
-        setActiveCollage(collages[collages.length - 1]);
-      } else {
-        setActiveCollage(collages[imageIndex - 1]);
-      }
-    }
-  };
+	const imageSlide = (next) => {
+		const imageIndex = collages.findIndex(
+			(co) => co.collageId === activeCollage.collageId
+		);
+		if (next) {
+			if (imageIndex === collages.length - 1) {
+				setActiveCollage(collages[0]);
+			} else {
+				setActiveCollage(collages[imageIndex + 1]);
+			}
+		} else {
+			if (imageIndex === 0) {
+				setActiveCollage(collages[collages.length - 1]);
+			} else {
+				setActiveCollage(collages[imageIndex - 1]);
+			}
+		}
+	};
 
-  return (
-    <Main>
-      <LaptopDiv>
-        <ImageList variant='masonry' cols={3} gap={8}>
-          {collages.map((collage, index) => (
-            <ImageListItem key={index}>
-              <ImageCard key={index} collage={collage} openModal={openModal} />
-            </ImageListItem>
-          ))}
-        </ImageList>
-      </LaptopDiv>
-      <TableDiv>
-        {newImageArray.map((imageSerie) => (
-          <Fragment key={imageSerie.serie}>
-            <div style={{ display: "flex", justifyContent: "center" }}>
-              <TitleH1>
-                {imageSerie.serie}, <span> </span>
-                {imageSerie.year}
-              </TitleH1>
-            </div>
-            <ImageSectionDiv>
-              <ImageSectionInnerDiv>
-                {imageSerie.collages?.map((collage, index) => (
-                  <>
-                    <ImageCard
-                      key={index}
-                      collage={collage}
-                      openModal={openModal}
-                    />
-                  </>
-                ))}
-              </ImageSectionInnerDiv>
-            </ImageSectionDiv>
-          </Fragment>
-        ))}
-      </TableDiv>
+	return (
+		<Main>
+			<LaptopDiv>
+				<ImageList variant='masonry' cols={3} gap={8}>
+					{collages.map((collage, index) => (
+						<ImageListItem key={index}>
+							<ImageCard key={index} collage={collage} openModal={openModal} />
+						</ImageListItem>
+					))}
+				</ImageList>
+			</LaptopDiv>
+			<TableDiv>
+				{newImageArray.map((imageSerie) => (
+					<Fragment key={imageSerie.serie}>
+						<div style={{ display: 'flex', justifyContent: 'center' }}>
+							<TitleH1>
+								{imageSerie.serie}, <span> </span>
+								{imageSerie.year}
+							</TitleH1>
+						</div>
+						<ImageSectionDiv>
+							<ImageSectionInnerDiv>
+								{imageSerie.collages?.map((collage, index) => (
+									<>
+										<ImageCard
+											key={index}
+											collage={collage}
+											openModal={openModal}
+										/>
+									</>
+								))}
+							</ImageSectionInnerDiv>
+						</ImageSectionDiv>
+					</Fragment>
+				))}
+			</TableDiv>
 
-      {showModal ? (
-        <Modal setShowModal={setShowModal} setActiveCollage={setActiveCollage}>
-          <ModalDiv>
-            <ArrowBack onClick={() => imageSlide(false)} />
+			{showModal ? (
+				<Modal setShowModal={setShowModal} setActiveCollage={setActiveCollage}>
+					<ModalDiv>
+						<ArrowBack onClick={() => imageSlide(false)} />
 
-            <ModalImage
-              alt='collage'
-              src={activeCollage?.collageImage?.file.url}
-            />
+						<ModalImage
+							alt='collage'
+							src={activeCollage?.collageImage?.file.url}
+						/>
 
-            <ArrowForward onClick={() => imageSlide(true)} />
-          </ModalDiv>
-        </Modal>
-      ) : (
-        <></>
-      )}
-    </Main>
-  );
+						<ArrowForward onClick={() => imageSlide(true)} />
+					</ModalDiv>
+				</Modal>
+			) : (
+				<></>
+			)}
+		</Main>
+	);
 };
 
 export default Collage;
