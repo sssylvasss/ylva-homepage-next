5ad0b8fdb00fc674fcf8168b46696f837be39e6b
sssylvasss <ylva.lan.lin@gmail.com>
2023-06-09 10:55:56 +0200

    updates

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,56 +1,56 @@
-import React from 'react';
+import React from "react";
 import {
-	Main,
-	Wrapper,
-	Nav,
-	Icon,
-	BottomTextWrapper,
-	Text,
-} from './StyleFooter';
+  Main,
+  Wrapper,
+  Nav,
+  Icon,
+  BottomTextWrapper,
+  Text,
+} from "./StyleFooter";
 
 export const Footer = () => {
-	return (
-		<Main>
-			<Wrapper>
-				<div>
-					<Text>Contact</Text>
-					<Text>Ylva Landoff Lindberg</Text>
-					<Text>+46 704 92 44 75</Text>
-					<Text>mail@ylvalandofflindberg.com</Text>
-				</div>
-				<Nav>
-					<a
-						target='_blank'
-						rel='noopener noreferrer'
-						tabindex='0'
-						href='https://github.com/sssylvasss'
-					>
-						<Icon src='../assets/github_black.svg' alt='Github linked icon' />
-					</a>
-					<a
-						target='_blank'
-						rel='noopener noreferrer'
-						tabindex='0'
-						href='https://www.linkedin.com/in/ylva-landoff-lindberg/'
-					>
-						<Icon
-							src='../assets/linkedin_black.svg'
-							alt='Linkdin linked icon'
-						/>
-					</a>
-					<a
-						target='_blank'
-						rel='noopener noreferrer'
-						tabindex='0'
-						href='mailto:mail@ylvalandofflindberg.com'
-					>
-						<Icon src='../assets/email_black.svg' alt='Mail linked icon' />
-					</a>
-				</Nav>
-			</Wrapper>
-			<BottomTextWrapper>
-				<Text> Website Developed by Ylva Landoff Lindberg</Text>
-			</BottomTextWrapper>
-		</Main>
-	);
+  return (
+    <Main>
+      <Wrapper>
+        <div>
+          <Text>Contact</Text>
+          <Text>Ylva Landoff Lindberg</Text>
+          <Text>+46 704 92 44 75</Text>
+          <Text>mail@ylvalandofflindberg.com</Text>
+        </div>
+        <Nav>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            tabIndex='1'
+            href='https://github.com/sssylvasss'
+          >
+            <Icon src='../assets/github_black.svg' alt='Github linked icon' />
+          </a>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            tabIndex='2'
+            href='https://www.linkedin.com/in/ylva-landoff-lindberg/'
+          >
+            <Icon
+              src='../assets/linkedin_black.svg'
+              alt='Linkdin linked icon'
+            />
+          </a>
+          <a
+            target='_blank'
+            rel='noopener noreferrer'
+            tabIndex='3'
+            href='mailto:mail@ylvalandofflindberg.com'
+          >
+            <Icon src='../assets/email_black.svg' alt='Mail linked icon' />
+          </a>
+        </Nav>
+      </Wrapper>
+      <BottomTextWrapper>
+        <Text> Website Developed by Ylva Landoff Lindberg</Text>
+      </BottomTextWrapper>
+    </Main>
+  );
 };
