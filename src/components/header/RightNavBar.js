80087aa49ff9f3cec123f32a6dad6f24cd28bd98
sssylvasss <ylva.lan.lin@gmail.com>
2023-06-09 10:30:58 +0200

    removed sapa

--- src/components/header/RightNavBar.js
+++ src/components/header/RightNavBar.js
@@ -1,22 +1,22 @@
-import Link from 'next/link';
-import React from 'react';
-import { Ul } from './styleHeader';
+import Link from "next/link";
+import React from "react";
+import { Ul } from "./styleHeader";
 
 export const RightNavBar = (props) => {
-	return (
-		<Ul open={props.open}>
-			<Link href={'/'} passHref>
-				<li onClick={() => props.setOpen(false)}>Collage</li>
-			</Link>
-			<Link href={'/video'} passHref>
-				<li onClick={() => props.setOpen(false)}>Video</li>
-			</Link>
-			<Link href={'/cv'} passHref>
-				<li onClick={() => props.setOpen(false)}>Bio</li>
-			</Link>
-			<Link href={'/sapasisters'} passHref>
+  return (
+    <Ul open={props.open}>
+      <Link href={"/"} passHref>
+        <li onClick={() => props.setOpen(false)}>Collage</li>
+      </Link>
+      <Link href={"/video"} passHref>
+        <li onClick={() => props.setOpen(false)}>Video</li>
+      </Link>
+      <Link href={"/cv"} passHref>
+        <li onClick={() => props.setOpen(false)}>Bio</li>
+      </Link>
+      {/* <Link href={'/sapasisters'} passHref>
 				<li onClick={() => props.setOpen(false)}>SAPA</li>
-			</Link>
-		</Ul>
-	);
+			</Link> */}
+    </Ul>
+  );
 };
