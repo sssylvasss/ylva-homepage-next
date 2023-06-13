45ba2135a78f673178381e15060e292a8bfa4147
sssylvasss <ylva.lan.lin@gmail.com>
2023-06-13 10:41:22 +0200

    updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import "@/styles/globals.css";
 import Layout from "../components/Layout";
-import { Roboto } from "@next/font/google";
+import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
   weight: "300",
