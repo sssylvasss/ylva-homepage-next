2811388623d1ced3c6595b8f5b98eae497cb0ac9
Ylva Landoff Lindberg <ylva.lan.lin@gmail.com>
2023-06-15 12:04:43 +0200

    acess test

--- src/useContentful.js
+++ src/useContentful.js
@@ -1,66 +1,73 @@
-import { createClient } from "contentful";
+import { createClient } from 'contentful';
 
 export const useContentful = () => {
-  const client = createClient({
-    space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-    accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_API,
-    host: process.env.NEXT_PUBLIC_CONTENTFUL_URL,
-  });
+	// const client = createClient({
+	//   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+	//   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_API,
+	//   host: process.env.NEXT_PUBLIC_CONTENTFUL_URL,
+	// });
+	const client = createClient({
+		space: process.env.CONTENTFUL_SPACE_ID,
+		accessToken:
+			process.env.IS_PREVIEW === 'true'
+				? process.env.CONTENTFUL_PREVIEW_TOKEN
+				: process.env.CONTENTFUL_DELIVERY_TOKEN,
+	});
 
-  const getCollage = async () => {
-    try {
-      const entries = await client.getEntries({
-        content_type: "spiritOfVietnam",
-        select: "fields",
-      });
-      const filteredEntries = entries.items.map((item) => {
-        const collageImage = item.fields.image.fields;
+	const getCollage = async () => {
+		try {
+			const entries = await client.getEntries({
+				content_type: 'spiritOfVietnam',
+				select: 'fields',
+			});
+			const filteredEntries = entries.items.map((item) => {
+				const collageImage = item.fields.image.fields;
 
-        return {
-          ...item.fields,
-          collageImage,
-        };
-      });
-      return filteredEntries;
-    } catch (err) {
-      console.log(`error fetching data": ${err}`);
-    }
-  };
+				return {
+					...item.fields,
+					collageImage,
+				};
+			});
+			return filteredEntries;
+		} catch (err) {
+			console.log(`error fetching data": ${err}`);
+		}
+	};
 
-  const getCv = async () => {
-    try {
-      const entries = await client.getEntries({
-        content_type: "cv",
-        select: "fields",
-      });
-      const filteredEntries = entries.items.map((item) => {
-        return {
-          ...item.fields,
-        };
-      });
-      return filteredEntries;
-    } catch (err) {
-      console.log(`error fetching data": ${err}`);
-    }
-  };
-  const getVideo = async () => {
-    try {
-      const entries = await client.getEntries({
-        content_type: "video",
-        select: "fields",
-      });
-      const filteredEntries = entries.items.map((item) => {
-        const videoImage = item.fields.videoImage.fields;
-        return {
-          ...item.fields,
-          videoImage,
-        };
-      });
-      return filteredEntries;
-    } catch (err) {
-      console.log(`error fetching data": ${err}`);
-    }
-  };
+	const getCv = async () => {
+		try {
+			const entries = await client.getEntries({
+				content_type: 'cv',
+				select: 'fields',
+			});
+			const filteredEntries = entries.items.map((item) => {
+				return {
+					...item.fields,
+				};
+			});
+			return filteredEntries;
+		} catch (err) {
+			console.log(`error fetching data": ${err}`);
+		}
+	};
+	const getVideo = async () => {
+		try {
+			const entries = await client.getEntries({
+				content_type: 'video',
+				select: 'fields',
+			});
+			const filteredEntries = entries.items.map((item) => {
+				const videoImage = item.fields.videoImage.fields;
+				return {
+					...item.fields,
+					videoImage,
+				};
+			});
+			return filteredEntries;
+		} catch (err) {
+			console.log(`error fetching data": ${err}`);
+		}
+	};
 
-  return { getCollage, getCv, getVideo };
+	return { getCollage, getCv, getVideo };
 };
