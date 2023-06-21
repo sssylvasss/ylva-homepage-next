8f8454e9390030b134390d77db058141ac8d8ee0
sssylvasss <ylva.lan.lin@gmail.com>
2023-06-21 08:53:07 +0200

    viewport meta

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -9,6 +9,7 @@ export default function Document() {
           href='https://fonts.googleapis.com/css2?family=Roboto:wght@300;500&display=swap'
           rel='stylesheet'
         />
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       </Head>
       <body>
         <Main />
