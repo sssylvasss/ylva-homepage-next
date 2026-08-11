243da2c33a8d65bcc8d7c79439e2140b83e2d561
sssylvasss <ylva.lan.lin@gmail.com>
2026-08-11 17:12:34 +0200

    added missing "use client"

--- src/components/header/Burger.tsx
+++ src/components/header/Burger.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { StyledBurger } from "./styles/Burger.styles";
 
--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import { ModalDiv, ModalInnerDiv, CloseIcon } from "./StyleModal";
 
